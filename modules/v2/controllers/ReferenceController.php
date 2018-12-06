9efe7a6c90e47b40f20021087d2a901876770d5e
Vladislav Alatorcev(Dangetsu) <prototype9696@mail.ru>
2018-12-06 23:27:58 +0300

    added required params to comment controller

--- modules/v2/controllers/ReferenceController.php
+++ modules/v2/controllers/ReferenceController.php
@@ -5,7 +5,52 @@
 
 namespace app\modules\v2\controllers;
 
+use app\modules\v2\models;
+use yii\web;
+use yii\base;
+
 class ReferenceController extends BaseController {
     /** @var string */
     public $modelClass = 'app\modules\v2\models\Reference';
+
+    /**
+     * @param int $id
+     * @return models\Reference
+     * @throws base\InvalidConfigException
+     * @throws web\ServerErrorHttpException
+     */
+    public function actionDelete($id) {
+        /** @var models\Reference $modelClass */
+        $modelClass = $this->modelClass;
+        $model = $modelClass::findOne(['id' => $id]);
+        $this->_saveModel($model, [$model::DELETED_LABEL => 1]);
+        return $model;
+    }
+
+    /**
+     * @param array $params
+     * @return array
+     */
+    protected function _prepareParamsForCreateItem(array $params = []) {
+        $result = parent::_prepareParamsForCreateItem($params);
+        /** @var models\Reference $model */
+        $model = new $this->modelClass();
+        $result[$model::USER_ID_LABEL] = \Yii::$app->user->getId() !== null ? \Yii::$app->user->getId() : 'NULL';
+        $result[$model::TIMESTAMP_LABEL] = (new \DateTime())->getTimestamp();
+        return $result;
+    }
+
+    /**
+     * @param array $params
+     * @return array
+     */
+    protected function _prepareParamsForUpdateItem(array $params = []) {
+        $result = parent::_prepareParamsForUpdateItem($params);
+        /** @var models\Reference $model */
+        $model = new $this->modelClass();
+        $result[$model::UPDATE_USER_ID_LABEL] = \Yii::$app->user->getId() !== null ? \Yii::$app->user->getId() : 'NULL';
+        $result[$model::UPDATE_TIMESTAMP_LABEL] = (new \DateTime())->getTimestamp();
+        unset($result[$model::USER_ID_LABEL], $result[$model::TIMESTAMP_LABEL]);
+        return $result;
+    }
 }
--- modules/v2/models/Reference.php
+++ modules/v2/models/Reference.php
@@ -6,14 +6,20 @@
 namespace app\modules\v2\models;
 
 class Reference extends Base {
+    const USER_ID_LABEL = 'user_id';
+    const DELETED_LABEL = 'deleted';
+    const UPDATE_USER_ID_LABEL = 'update_user_id';
+    const TIMESTAMP_LABEL = 'timestamp';
+    const UPDATE_TIMESTAMP_LABEL = 'update_timestamp';
+
     /**
      * @return array
      */
     public function rules() {
         return [
             [['type', 'user_hash', 'key', 'url', 'title', 'brand', 'comment', 'update_comment'], 'string'],
-            [['item_id', 'game_id', 'user_id', 'update_user_id', 'timestamp', 'update_timestamp', 'date'], 'integer'],
-            [['disabled', 'deleted'], 'boolean'],
+            [['item_id', 'game_id', self::USER_ID_LABEL, self::UPDATE_USER_ID_LABEL, self::TIMESTAMP_LABEL, self::UPDATE_TIMESTAMP_LABEL, 'date'], 'integer'],
+            [['disabled', self::DELETED_LABEL], 'boolean'],
         ];
     }
 }
